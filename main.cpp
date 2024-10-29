d705ebbe72b0f00d13e160a37c6f802de29d4269
ranisonitaliano <ranisonitaliano35@gmail.com>
2024-10-29 00:26:42 +0000

    comentario

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+   int idade;
+   cout << "Digite sua idade: ";
+   cin >> idade;
+   if(idade >= 18){
+    cout << "Você pode ser preso";
+   }
+   else{
+    cout << "Você vai pra FASE";
+   }
+   
+    return 0;
+    }
